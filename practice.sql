c378337ddc0eb40ea626a2ddb7a3fb9aa0a7bfa6
AlecTech <eremeevoleg@gmail.com>
2020-10-09 20:17:20 -0600

    2nd and 3rd Query

--- practice.sql
+++ practice.sql
@@ -6,3 +6,17 @@ WHERE contact.ContactID = contactcreditcard.ContactID AND contactcreditcard.Cred
 AND contact.FirstName = 'Carla' AND contact.LastName='Adams';
 
 -- 2.	Query: Print all of the addresses for customer with account number AW00000222
+
+SELECT address.AddressID, address.AddressLine1
+FROM address , customer, customeraddress
+WHERE address.AddressID = customeraddress.AddressID AND customer.CustomerID = customeraddress.CustomerID 
+AND customer.AccountNumber = 'AW00000222';
+
+-- 3.	Query: Print the city of the customer with customer ID number 40
+
+SELECT customer.CustomerID, address.AddressID, address.City
+FROM address , customer, customeraddress
+WHERE customer.CustomerID = customeraddress.CustomerID AND customeraddress.AddressID = address.AddressID 
+AND customer.CustomerID = 40;
+
+
